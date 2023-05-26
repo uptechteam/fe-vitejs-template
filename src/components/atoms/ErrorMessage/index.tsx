abbcfdf194b1cd9e95f233c10d487e1cadabe745
Viacheslav Miroshko <viacheslav.miroshko@uptech.team>
2023-05-26 11:44:27 +0300

    feat(FEC-14): added error boundary

---
+++ src/components/atoms/ErrorMessage/index.tsx
@@ -0,0 +1,8 @@
+export const ErrorMessage = () => {
+  return (
+    <>
+      <h2>Oops</h2>
+      <p>Something went wrong.</p>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,27 @@ import 'react-toastify/dist/ReactToastify.min.css';
 
 import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { ToastContainer } from 'react-toastify';
 
+import { ErrorMessage } from '~/components/atoms';
 import { App } from '~/components/atoms/App';
 import { LoadingMessage } from '~/components/atoms/LoadingMessage';
 
 createRoot(document.getElementById('root')).render(
   <Suspense fallback={<LoadingMessage />}>
-    <App />
-    <ToastContainer
-      position="top-center"
-      autoClose={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      theme="light"
-    />
+    <ErrorBoundary FallbackComponent={ErrorMessage} key={location.pathname}>
+      <App />
+      <ToastContainer
+        position="top-center"
+        autoClose={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="light"
+      />
+    </ErrorBoundary>
   </Suspense>
 );
