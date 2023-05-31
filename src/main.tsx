cc27d96d0500cc24f1bcf752bb9a4a0dc051220f
Maksym Kalashnyk <maxim.kalashnyk@gmail.com>
2023-05-31 12:10:45 +0300

    feat(FEC-14): added react toastify library (#4)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
+import 'react-toastify/dist/ReactToastify.min.css';
+
 import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 
 import { App } from '~/components/atoms/App';
 import { LoadingMessage } from '~/components/atoms/LoadingMessage';
@@ -7,5 +10,15 @@ import { LoadingMessage } from '~/components/atoms/LoadingMessage';
 createRoot(document.getElementById('root')).render(
   <Suspense fallback={<LoadingMessage />}>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      theme="light"
+    />
   </Suspense>
 );
