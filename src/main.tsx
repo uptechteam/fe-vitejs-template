89cbc85dd9b2ecd989f62ad18836f094788f9d6d
Viacheslav Miroshko <viacheslav.miroshko@uptech.team>
2023-06-01 14:55:24 +0300

    fix(FEC-14): added toastify styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
